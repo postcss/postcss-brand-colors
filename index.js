6bc75a6cdc5115dc99d899d02f6eccb4920e5dc3
Andrey Sitnik <andrey@sitnik.ru>
2015-02-06 10:43:08 +0300

    Add code to replace names to colors

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+var brands = require('brand-colors').getAll();
+
+var colors = { }
+for ( var i = 0; i < brands.length; i++ ) {
+    colors[ brands[i].name ] = brands[i].color;
+}
+
 module.exports = function (css) {
+    css.replaceValues(/([\d\w-]+)-color/g, { fast: '-color' },
+        function (all, name) {
+            var color = colors[name];
+            if ( color ) {
+                return color;
+            } else {
+                return all;
+            }
+        });
 };
--- test/test.js
+++ test/test.js
@@ -9,4 +9,17 @@ var test = function (input, output) {
 
 describe('postcss-brand-colors', function () {
 
+    it('replaces brands to colors', function () {
+        test('a { color: twitter-color }', 'a { color: #55acee }');
+    });
+
+    it('knows brands with difficult name', function () {
+        test('a { color: paypal-dark-2-color }', 'a { color: #3b7bbf }');
+    });
+
+    it('replaces many colors in one value', function () {
+        test('a { background: linear-gradient(ebay-color, ebay-2-color) }',
+             'a { background: linear-gradient(#e53238, #0064d2) }');
+    });
+
 });
