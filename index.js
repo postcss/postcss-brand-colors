a0b3534f2dda0986ea7042bebebd1973637da653
Andrey Sitnik <andrey@sitnik.ru>
2015-05-04 15:33:47 +0300

    Use new PostCSS API

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-var brands = require('brand-colors').getAll();
+var postcss = require('postcss');
+var brands  = require('brand-colors').getAll();
 
 var colors = { };
 for ( var i = 0; i < brands.length; i++ ) {
     colors[ brands[i].name ] = brands[i].color;
 }
 
-module.exports = function (css) {
-    css.replaceValues(/([\d\w-]+)-color/g, { fast: '-color' },
-        function (all, name) {
-            var color = colors[name];
-            if ( color ) {
-                return color;
-            } else {
-                return all;
-            }
-        });
-};
+module.exports = postcss.plugin('postcss-brand-colors', function () {
+    return function (css) {
+        css.replaceValues(/([\d\w-]+)-color/g, { fast: '-color' },
+            function (origin, name) {
+                var color = colors[name];
+                if ( color ) {
+                    return color;
+                } else {
+                    return origin;
+                }
+            });
+    };
+});
